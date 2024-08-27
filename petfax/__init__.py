f7d5e542dd6493f027bd8b20028d1a1f56985173
JesusMLares <twinlares2@hotmail.com>
2024-08-26 20:42:43 -0700

    Completed Activity 6: Database-Integration-with-Flask

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,19 @@
 # config                    
 from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
