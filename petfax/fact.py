cffaf222cb0f242ad12d96c174fcfeedc8e24045
JesusMLares <twinlares2@hotmail.com>
2024-08-26 20:22:26 -0700

    Completed activity 6

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,14 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET', 'POST']) 
+def index(): 
+    if request.method == 'POST':
+       print(request.form)
+       return render_template('facts/index.html')
+    
+    return 'This is the facts index'
 
 @bp.route('/new')
 def new(): 
